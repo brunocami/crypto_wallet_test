f876282ed455053b491d685ecdc20af1281c4a74
brunocami <bruno.cami@hotmail.com>
2023-03-08 16:03:15 -0300

    update home page

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from '../Dashboard';
+import Wallet from '../Wallets';
+import Transfer from '../Transfer'
+import Sidebar from '../Sidebar';
+import './style.css'
 
-function Home() {
+const Home = () => {
   return (
-    <div>Hello wolrd</div>
-  )
-}
+    <nav className="navbar_container" >
+      <div className="nav_section_2">
+        <div className='user_info_container'>
+          <div className='balance_info'>
+            <p>
+              Total Balance
+            </p>
+          </div>
+          <div className='user_info'>
+            <img src="/images/user.png" alt="" />
+            <p>
+              Welcome Back <br />
+              Bruno Cami
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="nav_section_1">
+        <Sidebar />
+      </div>
 
-export default Home
+      <div className="nav_section_3">
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/myWallet" element={<Wallet />} />
+            <Route exact path="/transfer" element={<Transfer />} />
+          </Routes>
+      </div>
+    </nav>
+  );
+};
+
+export default Home;
